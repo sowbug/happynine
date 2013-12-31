350a6da4afdc6b9b9fab407e011e61a0b12575e0
Mike Tsao <mike@sowbug.com>
2013-12-31 15:32:19 -0800

    Separate hideLoading() out for easier local development.

--- chrome_app/wallet.js
+++ chrome_app/wallet.js
@@ -1,73 +1,77 @@
 function moduleDidLoad() {
-    common.hideModule();
-    $("#loading-container").slideUp(250, function() {
-        $("#main-container").fadeIn(500, function() {
-            $('#main-tabs a:first').tab('show');
-        });
+  common.hideModule();
+  hideLoading();
+}
+
+function hideLoading() {
+  $("#loading-container").slideUp(250, function() {
+    $("#main-container").fadeIn(500, function() {
+      $('#main-tabs a:first').tab('show');
     });
+  });
 }
 
 function updateFingerprintImage(fingerprint) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://robohash.org/' + fingerprint +
-             '.png?set=set3&bgset=any&size=64x64', true);
-    xhr.responseType = 'blob';
-    xhr.onload = function(e) {
-        var img = document.querySelector("#fingerprint-img");
-        img.src = window.webkitURL.createObjectURL(this.response);
-    };
-    xhr.send();
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://robohash.org/' + fingerprint +
+   '.png?set=set3&bgset=any&size=64x64', true);
+  xhr.responseType = 'blob';
+  xhr.onload = function(e) {
+    var img = document.querySelector("#fingerprint-img");
+    img.src = window.webkitURL.createObjectURL(this.response);
+  };
+  xhr.send();
 }
 
 function handleMessage(message) {
-    var message_object = JSON.parse(message.data);
-    console.log(message);
-    switch (message_object.command) {
+  var message_object = JSON.parse(message.data);
+  console.log(message);
+  switch (message_object.command) {
     case "get-wallet-node":
-        document.querySelector("#ext-prv-b58").value =
-            message_object.ext_prv_b58;
-        document.querySelector("#chain-code").value =
-            message_object.chain_code;
-        document.querySelector("#public-key").value =
-            message_object.public_key;
-        document.querySelector("#fingerprint").value =
-            message_object.fingerprint;
-        updateFingerprintImage(message_object.fingerprint);
-        break;
+    document.querySelector("#ext-prv-b58").value =
+    message_object.ext_prv_b58;
+    document.querySelector("#chain-code").value =
+    message_object.chain_code;
+    document.querySelector("#public-key").value =
+    message_object.public_key;
+    document.querySelector("#fingerprint").value =
+    message_object.fingerprint;
+    updateFingerprintImage(message_object.fingerprint);
+    break;
     case "create-random-node":
-        document.querySelector("#path").value = "m";
-        document.querySelector("#ext-prv-b58").value =
-            message_object.ext_prv_b58;
-        document.querySelector("#chain-code").value =
-            message_object.chain_code;
-        document.querySelector("#public-key").value =
-            message_object.public_key;
-        document.querySelector("#fingerprint").value =
-            message_object.fingerprint;
-        updateFingerprintImage(message_object.fingerprint);
-        break;
-    }
+    document.querySelector("#path").value = "m";
+    document.querySelector("#ext-prv-b58").value =
+    message_object.ext_prv_b58;
+    document.querySelector("#chain-code").value =
+    message_object.chain_code;
+    document.querySelector("#public-key").value =
+    message_object.public_key;
+    document.querySelector("#fingerprint").value =
+    message_object.fingerprint;
+    updateFingerprintImage(message_object.fingerprint);
+    break;
+  }
 }
 
 window.onload = function() {
-    var onTabClick = function (e) {
-        e.preventDefault()
-        $(this).tab('show')
-    };
-    $('#main-tabs a').click(onTabClick);
+  var onTabClick = function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  };
+  $('#main-tabs a').click(onTabClick);
 
-    document.querySelector('#get-wallet-node').onclick = function() {
-        var message = {
-            'command': 'get-wallet-node',
-            'seed_hex': document.querySelector("#seed").value,
-            'path': document.querySelector("#path").value
-        };
-        common.naclModule.postMessage(JSON.stringify(message));
+  document.querySelector('#get-wallet-node').onclick = function() {
+    var message = {
+      'command': 'get-wallet-node',
+      'seed_hex': document.querySelector("#seed").value,
+      'path': document.querySelector("#path").value
     };
-    document.querySelector('#create-random-node').onclick = function() {
-        var message = {
-            'command': 'create-random-node'
-        };
-        common.naclModule.postMessage(JSON.stringify(message));
+    common.naclModule.postMessage(JSON.stringify(message));
+  };
+  document.querySelector('#create-random-node').onclick = function() {
+    var message = {
+      'command': 'create-random-node'
     };
+    common.naclModule.postMessage(JSON.stringify(message));
+  };
 };
