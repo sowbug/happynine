6b2a7a7300abb83b4b6ab9abd58fde63c884f052
Mike Tsao <mike@sowbug.com>
2014-01-01 12:20:32 -0800

    interim

--- chrome_app/wallet.js
+++ chrome_app/wallet.js
@@ -1,3 +1,49 @@
+var callbacks = {};
+var callbackId = 1;
+
+var bitcoinWalletApp = angular.module('bitcoinWalletApp', []);
+
+var postMessageWithCallback = function(message, callback) {
+  message.id = callbackId++;
+  callbacks[message.id] = callback;
+  common.naclModule.postMessage(JSON.stringify(message));
+};
+
+function WalletController($scope) {
+  $scope.addresses = [
+  { 'address': '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa', 'balance': '123.45', 'tx_count': 3 },
+  { 'address': '1JKMcZibd5MBn3sn4u3WVnFvHWMFiny59', 'balance': '3.50', 'tx_count': 1 },
+  { 'address': '1ADc4zojfRp9794ZX3ozDSGF4W2meSfkzr', 'balance': '0.0001', 'tx_count': 2 },
+  { 'address': '18gaduiJeXLcLJVcdhRAiMLge6YmCyAnQz', 'balance': '5.55', 'tx_count': 3 },
+  { 'address': '1CSWk7EioDwpqUDr4gftsjnzewPUSRPs9p', 'balance': '0.00', 'tx_count': 0 },
+  ];
+  $scope.transactions = [
+  { 'date': '3 Jan 2009', 'address': '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa', 'amount': '2.34' },
+  { 'date': '4 Jan 2009', 'address': '1JKMcZibd5MBn3sn4u3WVnFvHWMFiny59', 'amount': '50.00' },
+  { 'date': '5 Jan 2009', 'address': '17sz256snXYak5VMX8EdE4p4Pab8X8iMGn', 'amount': '(50.00)' }
+  ];
+  $scope.masterKey = {};
+  $scope.masterKey.xprv = "xprvfoobarbaz";
+  $scope.masterKey.showXprv = false;
+  $scope.masterKey.xpub = "xpubfoobarbaz";
+  $scope.masterKey.fingerprint = "0x12345678";
+
+  $scope.newMasterKey = function() {
+    var message = {
+      'command': 'create-random-node'
+    };
+    postMessageWithCallback(message, function(response) {
+      $scope.masterKey = {};
+      $scope.masterKey.xprv = response.ext_prv_b58;
+      $scope.masterKey.showXprv = false;
+      $scope.masterKey.xpub = response.ext_pub_b58;
+      $scope.masterKey.fingerprint = response.fingerprint;
+      //    updateFingerprintImage(message_object.fingerprint);
+    });
+    common.naclModule.postMessage(JSON.stringify(message));
+  };
+}
+
 function moduleDidLoad() {
   common.hideModule();
   hideLoading();
@@ -55,36 +101,16 @@ var refreshTransactionTable = function(transactions) {
        "</tr>"));
     }
     table.append(tbody);
-
   }
 };
 
 function handleMessage(message) {
   var message_object = JSON.parse(message.data);
   console.log(message);
-  switch (message_object.command) {
-    case "get-wallet-node":
-    document.querySelector("#ext-prv-b58").value =
-    message_object.ext_prv_b58;
-    document.querySelector("#chain-code").value =
-    message_object.chain_code;
-    document.querySelector("#public-key").value =
-    message_object.public_key;
-    document.querySelector("#fingerprint").value =
-    message_object.fingerprint;
-    updateFingerprintImage(message_object.fingerprint);
-    break;
-    case "create-random-node":
-    document.querySelector("#xprv-readonly").value =
-    message_object.ext_prv_b58;
-    document.querySelector("#xpub-readonly").value =
-    message_object.ext_pub_b58;
-    document.querySelector("#fingerprint-readonly").value =
-    message_object.fingerprint;
-    document.querySelector("#fingerprint-navbar").innerText =
-    message_object.fingerprint;
-    updateFingerprintImage(message_object.fingerprint);
-    break;
+  var id = message_object.id;
+  if (callbacks[id]) {
+    callbacks[id].call(message_object);
+    delete callbacks[id];
   }
 }
 
@@ -106,6 +132,27 @@ var onShowPrivateKeyClick = function(e) {
   $("#xprv-wrapper").slideToggle();
 };
 
+var setAccount = function(account) {
+  if (!account || account == 0) {
+    $("#account").text("Default Account");
+  } else {
+    $("#account").text("Account " + account);
+  }
+};
+
+var setMasterKey = function(key) {
+  if (!key) {
+ //   refreshTransactionTable([]);
+} else {
+  refreshTransactionTable([
+    { 'date': '3 Jan 2009', 'address': '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa', 'amount': '2.34' },
+    { 'date': '4 Jan 2009', 'address': '1JKMcZibd5MBn3sn4u3WVnFvHWMFiny59', 'amount': '50.00' },
+    { 'date': '5 Jan 2009', 'address': '17sz256snXYak5VMX8EdE4p4Pab8X8iMGn', 'amount': '(50.00)' }
+    ]);
+}
+setAccount();
+};
+
 window.onload = function() {
   // Add click handlers.
   $('#main-tabs a').click(onTabClick);
@@ -114,11 +161,7 @@ window.onload = function() {
 
   // Set up initial state.
   $("#xprv-wrapper").hide();
-  refreshTransactionTable([
-    { 'date': '3 Jan 2009', 'address': '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa', 'amount': '2.34' },
-    { 'date': '4 Jan 2009', 'address': '1JKMcZibd5MBn3sn4u3WVnFvHWMFiny59', 'amount': '50.00' },
-    { 'date': '5 Jan 2009', 'address': '17sz256snXYak5VMX8EdE4p4Pab8X8iMGn', 'amount': '(50.00)' }
-    ]);
+  setMasterKey();
 
   // document.querySelector('#get-wallet-node').onclick = function() {
   //   var message = {
