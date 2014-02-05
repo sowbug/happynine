89e203ba0b4b8efb55b3231c9e081a01cc08fde0
Mike Tsao <mike@sowbug.com>
2014-02-05 15:57:58 +0000

    Separate transactions from history in testing

--- nacl_module/blockchain_unittest.cc
+++ nacl_module/blockchain_unittest.cc
@@ -27,29 +27,27 @@
 #include "gtest/gtest.h"
 #include "test_constants.h"
 
-static bool TransactionsContain(const std::vector<const Transaction*>
+static bool TransactionsContain(const std::vector<const Transaction*>&
                                 transactions,
-                                const Blockchain::address_t& expected_addr,
-                                uint64_t expected_value) {
-// #if defined(BE_LOUD)
-//   std::cerr << "START HIST" << std::endl;
-//   for (history_t::const_iterator i = history.begin();
-//        i != history.end(); ++i) {
-//     std::cerr << "Hist: "
-//               << to_hex(i->tx_hash())
-//               << ": " << Base58::hash160toAddress(i->hash160())
-//               << ": " << i->value() << std::endl;
-//   }
-//   std::cerr << "END HIST" << std::endl;
-// #endif
+                                const bytes_t& tx_hash) {
+#if defined(BE_LOUD)
+  std::cerr << "---->" << std::endl;
+  for (std::vector<const Transaction*>::const_iterator
+         i = transactions.begin();
+       i != transactions.end(); ++i) {
+    std::cerr << to_hex((*i)->hash()) << std::endl;
+  }
+  std::cerr << "<----" << std::endl;
+#endif
 
-//   for (history_t::const_iterator i = history.begin();
-//        i != history.end(); ++i) {
-//     if (i->hash160() == expected_addr && i->value() == expected_value) {
-//       return true;
-//     }
-//   }
-//   return false;
+  for (std::vector<const Transaction*>::const_iterator
+         i = transactions.begin();
+       i != transactions.end(); ++i) {
+    if ((*i)->hash() == tx_hash) {
+      return true;
+    }
+  }
+  return false;
 }
 
 TEST(BlockchainTest, HappyPath) {
@@ -75,12 +73,6 @@ TEST(BlockchainTest, HappyPath) {
   EXPECT_EQ(50 * SATOSHIS_IN_BTC, blockchain->GetAddressBalance(ADDR_12c6));
   EXPECT_EQ(1, blockchain->GetAddressTxCount(ADDR_12c6));
 
-  // History
-  address_set.clear();
-  address_set.insert(ADDR_12c6);
-  blockchain->GetTransactionsForAddresses(address_set, transactions);
-  EXPECT_TRUE(TransactionsContain(transactions, ADDR_12c6, 50 * SATOSHIS_IN_BTC));
-
   // Get all unspent txos.
   Blockchain::address_set_t address_filter;
   tx_outs_t unspent_txos;
@@ -102,23 +94,11 @@ TEST(BlockchainTest, HappyPath) {
   blockchain->GetUnspentTxos(address_filter, unspent_txos);
   EXPECT_EQ(2, unspent_txos.size());
 
-  // History
-  address_set.clear();
-  address_set.insert(ADDR_199T);
-  blockchain->GetTransactionsForAddresses(address_set, transactions);
-  EXPECT_TRUE(TransactionsContain(transactions, ADDR_199T, 29000));
-
   blockchain->AddTransaction(TX_100D);
   blockchain->GetUnspentTxos(address_filter, unspent_txos);
   EXPECT_EQ(3, unspent_txos.size());
   EXPECT_EQ(14000, blockchain->GetAddressBalance(ADDR_1Guw));
   EXPECT_EQ(1, blockchain->GetAddressTxCount(ADDR_1Guw));
-
-  // History
-  address_set.clear();
-  address_set.insert(ADDR_1Guw);
-  blockchain->GetTransactionsForAddresses(address_set, transactions);
-  EXPECT_TRUE(TransactionsContain(transactions, ADDR_1Guw, 13000));
 
   blockchain->AddTransaction(TX_BFB1);
   blockchain->GetUnspentTxos(address_filter, unspent_txos);
@@ -135,8 +115,9 @@ TEST(BlockchainTest, HappyPath) {
   address_set.clear();
   address_set.insert(ADDR_1Guw);
   blockchain->GetTransactionsForAddresses(address_set, transactions);
-  EXPECT_TRUE(TransactionsContain(transactions, ADDR_1Guw, 13000));
-  EXPECT_TRUE(TransactionsContain(transactions, ADDR_1Guw, 13000));
+  EXPECT_EQ(2, transactions.size());
+  EXPECT_TRUE(TransactionsContain(transactions, TX_100D_HASH));
+  EXPECT_TRUE(TransactionsContain(transactions, TX_BFB1_HASH));
 }
 
 TEST(BlockchainTest, OutOfOrder) {
